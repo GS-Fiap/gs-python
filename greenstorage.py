21b3f4b3c6352886aa065f089feddf4564cbb178
PB369 <pedrohbarros369@gmail.com>
2023-05-29 14:25:27 -0300

    Green Storage 1.1

--- greenstorage.py
+++ greenstorage.py
@@ -1,28 +1,33 @@
 import re
 
 # Definindo as informações de acesso ao sistema:
-nome_usuario_salvo = ["Guilherme", "João", "Lucas", "Pedro", "Vinícius"]
+nome_usuario_salvo = ["Guilherme", "João", "Lucas", "Pedro", "Vinicius"]
 senha_sistema_salvo = "12345"
 
 # Definindo itens de exemplo no estoque:
-estoque = ["Uva", "13/07/2023", "125", "Suco de Laranja", "04/05/2023", "32", "Limão Verde", "31/12/2023", "204"]
+estoque = []
 
 continuar = "sim"
 
 # Criando uma função para cada funcionalidade do sistema:
 def previsao_demanda():
     somatoria = 0
-    nome_produto = input("\nMuito bem. Insira o nome do produto que deseja realizar este estudo:")
-    print("\nAgora, preciso que você insira a quantidade de meses que você deseja considerar e as vendas deste produto para cada um desses meses.")
-    tempo_meses = int(input("\nDigite quantos meses devem ser considerados: "))
-    print("\nInsira abaixo a quantidade de vendas para cada mês:\n")
-    # Solicitando um input para o tanto de meses que o usuário escolheu:
-    for i in range(1, tempo_meses+1, 1):
-        num_vendas = int(input(f"{i}° Mês: "))
-        somatoria += num_vendas
-    # Calculando a média aritimética para ser usada como previsão de demanda:
-    media = somatoria / tempo_meses
-    print(f"\nTendo em vista a quantidade de vendas nos últimos {tempo_meses} meses, espera-se que no próximo mês a demanda para {nome_produto} será de {media:.1f} unidades.")
+    nome_produto = input("\nMuito bem. Insira o nome do produto que deseja realizar este estudo:\n")
+    # Verificando se o produto não está no estoque e exibindo uma mensagem caso seja verdade:
+    if not nome_produto in estoque:
+        print("\nParece que este produto não está no estoque, tente registrá-lo antes de acessar esta opção.")
+        continuar = "sim"
+    else:
+        print("\nAgora, preciso que você insira a quantidade de meses que você deseja considerar e as vendas deste produto para cada um desses meses.")
+        tempo_meses = int(input("\nDigite quantos meses devem ser considerados: "))
+        print("\nInsira abaixo a quantidade de vendas para cada mês:\n")
+        # Solicitando um input para o tanto de meses que o usuário escolheu:
+        for i in range(1, tempo_meses+1, 1):
+            num_vendas = int(input(f"{i}° Mês: "))
+            somatoria += num_vendas
+        # Calculando a média aritimética para ser usada como previsão de demanda:
+        media = somatoria / tempo_meses
+        print(f"\nTendo em vista a quantidade de vendas nos últimos {tempo_meses} meses, espera-se que no próximo mês a demanda para {nome_produto} será de {media:.1f} unidades.")
 
 def validade_produtos():
     escolha = input("\nVocê deseja acessar a validade de um produto ESPECÍFICO ou de TODOS que estão no estoque?\n")
@@ -37,11 +42,16 @@ def validade_produtos():
             print(f"Data de validade: {estoque[i+1]}\n")
     elif escolha.lower() == "específico" or escolha.lower() == "especifico":
         nome_produto = input("\nE qual é o nome do produto que você deseja consultar?\n")
-        # Realizando a busca do item dentro do estoque e exibindo a sua data de validade:
-        for i in range(len(estoque)):
-                if nome_produto == estoque[i]:
-                    print(f"\nA data de validade do produto escolhido é esta: {estoque[i+1]}.")
-                    break
+        # Verificando se o produto não está no estoque e exibindo uma mensagem caso seja verdade:
+        if not nome_produto in estoque:
+            print("\nParece que este produto não está no estoque, tente registrá-lo antes de acessar esta opção.")
+            continuar = "sim"
+        else:
+            # Realizando a busca do item dentro do estoque e exibindo a sua data de validade:
+            for i in range(len(estoque)):
+                    if nome_produto == estoque[i]:
+                        print(f"\nA data de validade do produto escolhido é esta: {estoque[i+1]}.")
+                        break
 
 def tamanho_estoque():
     quantidade = 0
@@ -58,73 +68,84 @@ def tamanho_estoque():
 
     elif escolha.lower() == "específico" or escolha.lower() == "especifico":
         nome_produto = input("\nE qual é o nome do produto que você deseja consultar?\n")
+        # Verificando se o produto não está no estoque e exibindo uma mensagem caso seja verdade:
+        if not nome_produto in estoque:
+            print("\nParece que este produto não está no estoque, tente registrá-lo antes de acessar esta opção.")
+            continuar = "sim"
+        else:
+            # Realizando a busca do produto desejado dentro do estoque e exibindo a sua quantidade:
+            for i in range(len(estoque)):
+                    if nome_produto == estoque[i]:
+                        print(f"\nEste produto possui {estoque[i+2]} itens.")
+                        break
 
-        # Realizando a busca do produto desejado dentro do estoque e exibindo a sua quantidade:
-        for i in range(len(estoque)):
-                if nome_produto == estoque[i]:
-                    print(f"\nEste produto possui {estoque[i+2]} itens.")
-                    break
-
-def registrar_remover_produto():
+def escolha_registrar_remover_produto():
     escolha = input("\nCerto, você deseja ADICIONAR ou REMOVER um produto?\n")
     while  not escolha.lower() == "adicionar" and not escolha.lower() == "remover":
         print("Desculpe, mas não entendi a sua resposta. Você deseja ADICIONAR ou REMOVER um produto?")
         escolha = input()
     # Pedindo as informações necessárias para se adicionar um produto ao estoque:
     if escolha.lower() == "adicionar":
-        nome_produto = input("\nQual é nome do produto a ser adicionado?\n")
-        estoque.append(nome_produto)
-        print("\nE qual é a data de validade deste produto?")
-        dia_validade = input("\nDia: ")
-        while int(dia_validade) < 1 or int(dia_validade) > 31:
-            print("\nO valor inserido é inválido, verifique-o e tente novamente.")
-            dia_validade = input("\nDia: ")
-        # Adicionando um zero no inicio do número caso o usuário não o tenha inserido na digitação:
-        if dia_validade[0] != "0":
-            dia_validade = "0" + dia_validade
-
-        mes_validade = input("\nMês: ")
-        while int(mes_validade) < 1 or int(mes_validade) > 12:
-            print("\nO valor inserido é inválido, verifique-o e tente novamente.")
-            mes_validade = input("Mês: ")
-        # Adicionando um zero no inicio do número caso o usuário não o tenha inserido na digitação:
-        if mes_validade[0] != "0":
-            mes_validade = "0" + mes_validade
-
-        ano_validade = input("\nAno: ")
-        while int(len(ano_validade)) !=4 or int(ano_validade) < 2023:
-            print("\nO valor inserido é inválido ou está ultrapassado, verifique-o e tente novamente.")
-            ano_validade = input("\nAno: ")
-
-        data_validade = f"{dia_validade}/{mes_validade}/{ano_validade}"
-
-        estoque.append(data_validade)
-
-        print("\nQuantos itens você quer registrar para este produto?\n")
-        quantidade_itens = input("")
-        estoque.append(quantidade_itens)
-
-        print("\nProduto registrado no estoque com sucesso!")
-        print(estoque)
+        registrar_produto()
 
     elif escolha.lower() == "remover":
-        nome_produto = input("Qual é o nome do produto a ser removido do estoque?\n")
-        # Verificando se o produto não está no estoque e exibindo uma mensagem caso seja verdade:
-        while not nome_produto in estoque:
-            print("Parece que este produto não está no estoque, verifique o nome inserido e tente novamente.")
-            nome_produto = input("Digite novamente o nome do produto ou escreva SAIR para encerrar esta opção: \n")
-            if nome_produto.lower() == "sair":
-                break
-        if not nome_produto == "sair":
-            # Procurando o produto dentro do estoque e realizando a sua remoção:
-            for i in range(len(estoque)):
-                if nome_produto == estoque[i]:
-                    estoque.pop(i+2)
-                    estoque.pop(i+1)
-                    estoque.pop(i)
-                    break
+        remover_produto()
 
-            print(f"Produto removido do estoque com sucesso!")
+def registrar_produto():
+    nome_produto = input("\nQual é nome do produto a ser adicionado?\n")
+    estoque.append(nome_produto)
+    print("\nE qual é a data de validade deste produto?")
+    dia_validade = input("\nDia: ")
+    while int(dia_validade) < 1 or int(dia_validade) > 31:
+        print("\nO valor inserido é inválido, verifique-o e tente novamente.")
+    # Adicionando um zero no inicio do número caso o usuário não o tenha inserido na digitação:
+    if (re.search("\d{1}", dia_validade)) and not (re.search("\d{2}", dia_validade)):
+        dia_validade = "0" + dia_validade
+
+    mes_validade = input("\nMês: ")
+    while int(mes_validade) < 1 or int(mes_validade) > 12:
+        print("\nO valor inserido é inválido, verifique-o e tente novamente.")
+        mes_validade = input("Mês: ")
+    # Adicionando um zero no inicio do número caso o usuário não o tenha inserido na digitação:
+    if (re.search("\d{1}", mes_validade)) and not (re.search("\d{2}", mes_validade)):
+        mes_validade = "0" + mes_validade
+
+    ano_validade = input("\nAno: ")
+    while int(len(ano_validade)) != 4 or int(ano_validade) < 2023:
+        print("\nO valor inserido é inválido ou está ultrapassado, verifique-o e tente novamente.")
+        ano_validade = input("\nAno: ")
+
+    data_validade = f"{dia_validade}/{mes_validade}/{ano_validade}"
+
+    estoque.append(data_validade)
+
+    print("\nQuantos itens você quer registrar para este produto?\n")
+    quantidade_itens = input("")
+    estoque.append(quantidade_itens)
+
+    print("\nProduto registrado no estoque com sucesso!")
+    print(estoque)
+
+def remover_produto():
+    # Verificando se o estoque está vazio:
+        if estoque == []:
+            print("\nParece que você não tem nenhum produto registrado no estoque, tente fazer isto antes de acessar esta opção.")
+            repetir = True
+        else:
+            nome_produto = input("Qual é o nome do produto a ser removido do estoque?\n")
+            # Verificando se o produto não está no estoque e exibindo uma mensagem caso seja verdade:
+            if not nome_produto in estoque:
+                print("\nParece que este produto não está no estoque, tente registrá-lo antes de acessar esta opção.")
+            else:
+                # Procurando o produto dentro do estoque e realizando a sua remoção:
+                for i in range(len(estoque)):
+                    if nome_produto == estoque[i]:
+                        estoque.pop(i+2)
+                        estoque.pop(i+1)
+                        estoque.pop(i)
+                        break
+
+                print(f"Produto removido do estoque com sucesso!")
 
 print("Seja bem-vindo ao seu assistente!")
 
@@ -144,12 +165,11 @@ try:
             else:
                 break
 
-
     print(f"\nOlá, {nome_usuario}.")
-    while continuar == "sim":
+    while continuar.lower() == "sim":
         print("\nComo posso ajudá-lo?")
         # Exibindo as opções de funcionalidades do sistema:
-        print("\n1-Quero fazer uma previsão de demanda\n2-Quero acessar a validade dos meus produtos\n3-Quero saber o tamanho do meu estoque\n4-Quero registrar um novo produto ou remover um produto do estoque\n")
+        print("\n1 - Quero fazer uma previsão de demanda\n2 - Quero acessar a validade dos meus produtos\n3 - Quero saber o tamanho do meu estoque\n4 - Quero registrar um novo produto ou remover um produto do estoque\n")
         escolha = int(input())
 
         while not (escolha == 1 or escolha == 2 or escolha == 3 or escolha == 4):
@@ -157,8 +177,13 @@ try:
             escolha = int(input())
         # Chamando a função de cada funcionalidade de acordo com a escolha do usuário:
         if escolha == 1:
-            continuar = "não"
-            previsao_demanda()
+            # Verificando se o estoque está vazio:
+            if estoque == []:
+                print("\nParece que você não tem nenhum produto registrado no estoque, tente fazer isto antes de acessar esta opção.")
+                continue
+            else:
+                continuar = "não"
+                previsao_demanda()
         elif escolha == 2:
             if estoque == []:
                 print("\nParece que você não tem nenhum produto registrado no estoque, tente fazer isto antes de acessar esta opção.")
@@ -167,11 +192,14 @@ try:
                 continuar = "não"
                 validade_produtos()
         elif escolha == 3:
-            continuar = "não"
-            tamanho_estoque()
+            if estoque == []:
+                print("\nParece que você não tem nenhum produto registrado no estoque, tente fazer isto antes de acessar esta opção.")
+                continue
+            else:
+                continuar = "não"
+                tamanho_estoque()
         elif escolha == 4:
-            continuar = "não"
-            registrar_remover_produto()
+            escolha_registrar_remover_produto()
         # Perguntando ao usuário se ele deseja continuar ou não usando o sistema:
         continuar = input("\nEspero ter ajudado. Você deseja o meu auxílio em mais alguma coisa? (Sim/Não)\n")
 
---
+++ readme.txt
@@ -0,0 +1,23 @@
+INTEGRANTES:
+RM99499 – Guilherme Monteiro Espim
+RM99279 – João Paulo Fonseca Zamperlini
+RM98297 – Lucas Pisaneschi Speranzini
+RM97937 – Pedro Henrique Fernandes Lô de Barros
+RM97824 – Vinicius Oliveira de Barros
+
+SOBRE O PROJETO:
+O objetivo da nossa equipe é desenvolver um software que, que a partir de dados fornecidos pelas empresas e organizações que utilizarem este produto, seja capaz de gerenciar os estoques de alimento com a intenção de reduzir a compra de quantidades desnecessárias, que acabarão, posteriormente, sendo descartadas. Através da análise de vendas, previsões de demanda e a data de validade dos produtos, o sistema será capaz de fornecer informações precisas sobre a quantidade ideal para as empresas adquirirem.
+
+Este programa apresentado é um protótipo de nossa ideia. Ele foi desenvolvido pensando na fácil compreensão do código e com uma interface contendo uma linguagem mais humana, simulando um verdadeiro assistente virtual.
+
+INSTRUÇÃO BÁSICA DE USO:
+Ao instalar a pasta do projeto, o usuário deverá acessar o arquivo chamado "greenstorage.py". Tendo aberto este arquivo, o programa será executado automaticamente. O usuário poderá interagir com o assistente através da digitação de informações pelo teclado de seu computador, conforme se pede pelo sistema.
+
+REQUISITOS:
+Para que seja possível a utilização do assistente, o usuário necessitará ter o Python 3 instalado em seu computador.
+
+DEPENDÊNCIAS:
+As únicas dependências é ter o Python 3, conforme dito anteriormente.
+
+DEMAIS INFORMAÇÕES:
+
